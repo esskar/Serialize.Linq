4b0d5ff69d158707ad447f3f076bdf404ab46d85
esskar <sk@intertivity.com>
2012-09-11 18:25:18 +0200

    adding tests

---
+++ Serialize.Linq.Tests/SerializationHelperTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serialize.Linq.Internals;
+
+namespace Serialize.Linq.Tests
+{
+    [TestClass]
+    public class SerializationHelperTests
+    {
+        private static readonly string __serializationHelperTypeText = "Serialize.Linq.Internals.SerializationHelper, Serialize.Linq, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+        private static readonly string __serializationHelperSerializeMethodText = __serializationHelperTypeText + Environment.NewLine + "System.String SerializeMethod(System.Reflection.MethodInfo)";
+
+        [TestMethod]
+        public void SerializeTypeTest()
+        {
+            var actual = SerializationHelper.SerializeType(typeof(SerializationHelper));
+            Assert.AreEqual(__serializationHelperTypeText, actual);
+        }
+
+        [TestMethod]
+        public void SerializeNullTypeTest()
+        {
+            var actual = SerializationHelper.SerializeType(null);
+            Assert.AreSame(null, actual);
+        }
+
+        [TestMethod]
+        public void DeserializeTypeTest()
+        {
+            var actual = SerializationHelper.DeserializeType(__serializationHelperTypeText);
+            Assert.AreSame(typeof(SerializationHelper), actual);
+        }
+
+        [TestMethod]
+        public void DeserializeNullTypeTest()
+        {
+            var actual = SerializationHelper.DeserializeType(null);
+            Assert.AreSame(null, actual);
+        }
+
+        [TestMethod]
+        public void SerializeDeserializeAllSystemTypesTest()
+        {
+            foreach (var type in typeof(string).Assembly.GetTypes())            
+                Assert.AreSame(type, SerializationHelper.DeserializeType(SerializationHelper.SerializeType(type)));            
+        }
+
+        [TestMethod]
+        public void SerializeMethodTest()
+        {
+            var method = typeof(SerializationHelper).GetMethod("SerializeMethod");
+            var actual = SerializationHelper.SerializeMethod(method);
+            Assert.AreEqual(__serializationHelperSerializeMethodText, actual);
+        }
+
+        [TestMethod]
+        public void DeserializeMethodTest()
+        {
+            var method = typeof(SerializationHelper).GetMethod("SerializeMethod");
+            var actual = SerializationHelper.DeserializeMethod(__serializationHelperSerializeMethodText);
+            Assert.AreEqual(method, actual);
+        }
+    }
+}
--- Serialize.Linq/Properties/AssemblyInfo.cs
+++ Serialize.Linq/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
 [assembly: AssemblyTitle("Serialize.Linq")]
 [assembly: AssemblyProduct("Serialize.Linq")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Serialize.Linq.Tests")]
