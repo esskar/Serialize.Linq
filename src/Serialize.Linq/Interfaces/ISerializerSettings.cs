cef018612a55eb4e1268877666d46a114e4cc163
esskar <sk@intertivity.com>
2012-09-20 21:27:10 +0200

    removed some files

--- src/Serialize.Linq/Interfaces/ISerializerSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Serialize.Linq.Interfaces
-{
-    public interface ISerializerSettings
-    {
-        bool UseAssemblyQualifiedName { get; set; }
-
-        bool UseReferences { get; set; }
-    }
-}
