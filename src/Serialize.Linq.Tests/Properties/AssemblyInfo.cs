de7e2b83766175f57e12dfe244b3e2f2cb508849
esskar <sk@intertivity.com>
2012-12-30 23:54:57 +0100

    changed assembly version

--- src/Serialize.Linq.Tests/Properties/AssemblyInfo.cs
+++ src/Serialize.Linq.Tests/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 [assembly: AssemblyTitle("Serialize.Linq.Tests")]
 [assembly: AssemblyProduct("Serialize.Linq.Tests")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- src/Serialize.Linq/Properties/AssemblyInfo.cs
+++ src/Serialize.Linq/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Serialize.Linq")]
 [assembly: AssemblyProduct("Serialize.Linq")]
 [assembly: AssemblyCopyright("Copyright © 2012")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: InternalsVisibleTo("Serialize.Linq.Tests")]
