3dbdd1c52d515dd32930b20a09adf39dada6e069
esskar <sk@intertivity.com>
2013-05-10 12:37:22 +0200

    update for nuget version 1.1.4

--- src/Serialize.Linq/Properties/AssemblyInfo.cs
+++ src/Serialize.Linq/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Serialize.Linq")]
 [assembly: AssemblyProduct("Serialize.Linq")]
 [assembly: AssemblyCopyright("Copyright © 2012-2013")]
 [assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
 
 [assembly: CLSCompliant(true)]
