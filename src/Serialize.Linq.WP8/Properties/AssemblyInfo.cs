0d19bf9843a233235ca369498051d20bba6cde36
esskar <sk@intertivity.com>
2013-07-23 23:11:19 +0200

    fix assembly infos

--- src/Serialize.Linq.WP8/Properties/AssemblyInfo.cs
+++ src/Serialize.Linq.WP8/Properties/AssemblyInfo.cs
@@ -1,37 +1,22 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region Copyright
+//  Copyright, Sascha Kiefer (esskar)
+//  Released under LGPL License.
+//  
+//  License: https://raw.github.com/esskar/Serialize.Linq/master/LICENSE
+//  Contributing: https://github.com/esskar/Serialize.Linq
+#endregion
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Serialize.Linq.WP8")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Serialize.Linq.WP8")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyProduct("Serialize.Linq")]
+[assembly: AssemblyCopyright("Copyright © 2012-2013")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("269b6008-6ed9-4f09-9f81-1d1bbf7f9962")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
+
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
