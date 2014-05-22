380b784edd0d57f2ed3f973bf7a86a2cdbf07821
unknown <sk@intertivity.com>
2014-05-22 11:45:07 +0200

    adding WCF host and client example

---
+++ src/Serialize.Linq.Examples/Serialize.Linq.Examples.WcfContracts/Entities/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Serialize.Linq.Examples.WcfContracts.Entities
+{
+    [DataContract]
+    public enum Gender
+    {
+        [EnumMember]
+        Male,
+        [EnumMember]
+        Female
+    }
+}
---
+++ src/Serialize.Linq.Examples/Serialize.Linq.Examples.WcfHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+using Serialize.Linq.Examples.WcfHost.Services;
+
+namespace Serialize.Linq.Examples.WcfHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var host = new ServiceHost(typeof(PersonService)))
+            {
+                host.Open();
+
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");                
+                Console.ReadLine();                
+            }
+        }
+    }
+}
