feca0bd1fb3bb0182ed0fd0dd991e12d974d922e
esskar <sk@intertivity.com>
2014-12-05 15:07:07 +0100

    adding tests for issue #37

---
+++ src/Serialize.Linq.Tests/Issues/Issue37.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serialize.Linq.Interfaces;
+using Serialize.Linq.Serializers;
+using Serialize.Linq.Tests.Internals;
+
+namespace Serialize.Linq.Tests.Issues
+{
+    [TestClass]
+    public class Issue37
+    {
+        [TestMethod]
+        public void DynamicsTests()
+        {
+            var expressions = new List<Expression>();
+
+            Expression<Func<Item, dynamic>> objectExp = item => new {item.Name, item.ProductId};
+            Expression<Func<string, dynamic>> stringExp = str => new { Text = str };
+
+            expressions.Add(objectExp);
+            expressions.Add(stringExp);
+
+            foreach (var textSerializer in new ITextSerializer[] { new JsonSerializer(), new XmlSerializer() })
+            {
+                var serializer = new ExpressionSerializer(textSerializer);
+                foreach (var expected in expressions)
+                {
+                    var serialized = serializer.SerializeText(expected);
+                    var actual = serializer.DeserializeText(serialized);
+
+                    ExpressionAssert.AreEqual(expected, actual);
+                }
+            }
+        }
+
+        public class Item
+        {
+            public string Name { get; set; }
+
+            public string ProductId { get; set; }
+        }
+    }
+}
