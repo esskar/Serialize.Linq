828a14cfc9b1ea71038f90842d05822bf88e4f6e
vsoldatkin <vksoldatkin@gmail.com>
2015-02-10 15:28:34 +0300

    Added test case for an issue

---
+++ src/Serialize.Linq.Tests.TestContainerAssembly/Container.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialize.Linq.Tests.TestDesiredAssembly
+{
+    public class Container
+    {
+        public Expression GetExpression()
+        {
+            Expression<Func<IEnumerable<int>, IEnumerable<int>>> Expr = c =>
+                from x in c
+                let someConst_6547588C372F49698EC3B242C745FCA0 = 8
+                where (x == someConst_6547588C372F49698EC3B242C745FCA0)
+                select x;
+
+            return Expr;
+        }
+    }
+}
---
+++ src/Serialize.Linq.Tests/Issues/IssueTypeDiscovery.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Serialize.Linq.Extensions;
+using Serialize.Linq.Serializers;
+using Serialize.Linq.Tests.Internals;
+using Serialize.Linq.Interfaces;
+
+namespace Serialize.Linq.Tests.Issues
+{
+    /// <remarks>
+    ///   Serialize.Linq.Tests acts as an interfering assembly.
+    ///    
+    ///    
+    ///   Actually, another expression from this assembly (Serialize.Linq.Tests) affects
+    ///   execution results. In a couple of places it has similar expressions which have a "where" clause that 
+    ///   pushes it's predicate to a certain anonymous class. This class is later picked 
+    ///   while processing the expression under test, and cause an exception.
+    ///   </remarks>
+    [TestClass]
+    public class IssueTypeDiscovery
+    {
+        [TestMethod]
+        public void SerializeWithInterferingAssembly()
+        {
+            // Interfering expression
+            Expression<Func<IEnumerable<int>, IEnumerable<int>>> Expr = c =>
+                from x in c
+                let anotherConst = 100
+                where (x == anotherConst)
+                select x;
+
+            var DesiredContainer = new Serialize.Linq.Tests.TestDesiredAssembly.Container();
+
+            var Expression = ((Serialize.Linq.Tests.TestDesiredAssembly.Container)DesiredContainer).GetExpression();
+
+            foreach (var textSerializer in new ITextSerializer[] { new JsonSerializer(), new XmlSerializer() })
+            {
+                var serializer = new ExpressionSerializer(textSerializer);
+
+                var serialized = serializer.SerializeText(Expression);
+
+                var actual = serializer.DeserializeText(serialized);
+
+                ExpressionAssert.AreEqual(Expression, actual);
+            }
+        }
+
+        [TestMethod]
+        public void ToExpressionWithInterferingAssembly()
+        {
+            // Interfering expression
+            Expression<Func<IEnumerable<int>, IEnumerable<int>>> Expr = c =>
+                from x in c
+                let anotherConst = 100
+                where (x == anotherConst)
+                select x;
+
+            var DesiredContainer = new Serialize.Linq.Tests.TestDesiredAssembly.Container();
+
+            var Expression = ((Serialize.Linq.Tests.TestDesiredAssembly.Container)DesiredContainer).GetExpression();
+
+            var Node = Expression.ToExpressionNode();
+
+            var Result = Node.ToExpression();
+
+            ExpressionAssert.AreEqual(Expression, Result);
+        }
+    }
+}
