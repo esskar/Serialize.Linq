df4b73eee91c347c76b450eaf9222c8484635bc7
esskar <sk@intertivity.com>
2015-02-17 00:39:58 +0100

    add a test for issue #43

---
+++ src/Serialize.Linq.Tests/Issues/Issue43.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serialize.Linq.Extensions;
+
+namespace Serialize.Linq.Tests.Issues
+{
+    [TestClass]
+    public class Issue43
+    {
+        class User
+        {
+            public Guid Id
+            {
+                get;
+                set;
+            }
+        }
+
+        [TestMethod, Ignore]
+        public void ThrowsStackOverflowException()
+        {
+            var idCollection = new List<Guid>();
+            for (var i = 0; i < 15000; i++)
+            {
+                idCollection.Add(Guid.NewGuid());
+            }
+
+            var propertyInfo = typeof(User).GetProperty("Id");
+            var userParam = Expression.Parameter(typeof(User), "x");
+            var propertyExpression = Expression.Property(userParam, propertyInfo);
+            Expression composedExpression = Expression.Constant(false, typeof(bool));
+
+            composedExpression = idCollection.Select(t => Expression.MakeBinary(ExpressionType.Equal, propertyExpression, Expression.Constant(t))).Aggregate<Expression, Expression>(composedExpression, (current, equalExpression) => Expression.MakeBinary(ExpressionType.OrElse, equalExpression, current));
+            var expressionNode = composedExpression.ToExpressionNode();
+        }
+    }
+}
