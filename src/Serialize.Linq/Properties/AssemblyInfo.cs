df3e414f31917ae136139d0a1d0614d4af002177
esskar <sk@intertivity.com>
2015-02-23 10:24:27 +0100

    version 1.2.3

--- src/Serialize.Linq/Properties/AssemblyInfo.cs
+++ src/Serialize.Linq/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("Serialize.Linq")]
 [assembly: AssemblyProduct("Serialize.Linq")]
 [assembly: AssemblyCopyright("Copyright © 2012-2014")]
-[assembly: AssemblyVersion("1.2.2")]
-[assembly: AssemblyFileVersion("1.2.2")]
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
 
 [assembly: CLSCompliant(true)]
