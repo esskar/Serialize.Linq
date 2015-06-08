16ae2ac0469d885bf47825d8dadab6464627a9a4
esskar <sk@intertivity.com>
2015-06-08 21:53:49 +0200

    add seperate .net45 project (#48)

---
+++ src/Serialize.Linq.Net45/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+#region Copyright
+//  Copyright, Sascha Kiefer (esskar)
+//  Released under LGPL License.
+//  
+//  License: https://raw.github.com/esskar/Serialize.Linq/master/LICENSE
+//  Contributing: https://github.com/esskar/Serialize.Linq
+#endregion
+
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Serialize.Linq")]
+[assembly: AssemblyProduct("Serialize.Linq")]
+[assembly: AssemblyCopyright("Copyright © 2012-2014")]
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
+
+[assembly: CLSCompliant(true)]
