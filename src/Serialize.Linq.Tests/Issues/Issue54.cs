cc50a250f670725284c8247179a9f53c99376ec8
TDenis <dtimukhin@yandex.ru>
2015-08-10 21:30:47 +0300

    Unit tests for the Issue #54

---
+++ src/Serialize.Linq.Tests/Issues/Issue54.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serialize.Linq.Serializers;
+
+namespace Serialize.Linq.Tests.Issues
+{
+    [TestClass]
+    public class Issue54
+    {
+        // ReSharper disable InconsistentNaming
+        public int _publicField = 42;
+        private int _privateField = 42;
+        protected int _protectedField = 42;
+        protected internal int _protectedInternalField = 42;
+
+        private static int _privateStaticField = 42;
+        public static int _publicStaticField = 42;
+        protected static int _protectedStaticField = 42;
+        protected internal static int _protectedInternalStaticField = 42;
+        // ReSharper restore InconsistentNaming
+
+        [TestMethod]
+        public void SerializePublicField()
+        {
+            Expression<Func<Test, bool>> expression = test => test.IntProperty == _publicField;
+
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            var value = serializer.SerializeText(expression);
+
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void SerializePrivateField()
+        {
+            Expression<Func<Test, bool>> expression = test => test.IntProperty == _privateField;
+
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            var value = serializer.SerializeText(expression);
+
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void SerializeProtectedField()
+        {
+            Expression<Func<Test, bool>> expression = test => test.IntProperty == _protectedField;
+
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            var value = serializer.SerializeText(expression);
+
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void SerializeProtectedInternalField()
+        {
+            Expression<Func<Test, bool>> expression = test => test.IntProperty == _protectedInternalField;
+
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            var value = serializer.SerializeText(expression);
+
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void SerializePublicStaticField()
+        {
+            Expression<Func<Test, bool>> expression = test => test.IntProperty == _publicStaticField;
+
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            var value = serializer.SerializeText(expression);
+
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void SerializePrivateStaticField()
+        {
+            Expression<Func<Test, bool>> expression = test => test.IntProperty == _privateStaticField;
+
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            var value = serializer.SerializeText(expression);
+
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void SerializeProtectedStaticField()
+        {
+            Expression<Func<Test, bool>> expression = test => test.IntProperty == _protectedStaticField;
+
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            var value = serializer.SerializeText(expression);
+
+            Assert.IsNotNull(value);
+        }
+
+        [TestMethod]
+        public void SerializeProtectedInternalStaticField()
+        {
+            Expression<Func<Test, bool>> expression = test => test.IntProperty == _protectedInternalStaticField;
+
+            var serializer = new ExpressionSerializer(new JsonSerializer());
+            var value = serializer.SerializeText(expression);
+
+            Assert.IsNotNull(value);
+        }
+
+        public class Test
+        {
+            public int IntProperty { get; set; }
+        }
+    }
+}
