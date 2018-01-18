6f5ca3b097cc260d98bd527307f7fa12f3fa1947
Sascha Kiefer <esskar@gmail.com>
2018-01-17 23:33:21 -0800

    version 1.6.0

--- src/Serialize.Linq/Properties/AssemblyInfo.cs
+++ src/Serialize.Linq/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 [assembly: AssemblyTitle("Serialize.Linq")]
 [assembly: AssemblyProduct("Serialize.Linq")]
 [assembly: AssemblyCopyright("Copyright © 2012-2018")]
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
 
 [assembly: CLSCompliant(true)]
