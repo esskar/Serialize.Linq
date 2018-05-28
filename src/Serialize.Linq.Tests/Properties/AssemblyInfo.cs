296d08399f3ddc1868612d0620676fdafe018dce
Sascha Kiefer <esskar@gmail.com>
2018-05-28 11:46:14 +0200

    migrate to dotnet tests

--- src/Serialize.Linq.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("Serialize.Linq.Tests")]
-[assembly: AssemblyProduct("Serialize.Linq.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2012-2017")]
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
