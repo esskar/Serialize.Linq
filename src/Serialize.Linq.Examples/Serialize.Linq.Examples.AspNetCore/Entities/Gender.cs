c4c499c4074c3d1460b73269d720ea6c05da3f09
Sascha Kiefer <esskar@gmail.com>
2019-03-21 14:20:38 +0100

    #111 add aspnet core example project

---
+++ src/Serialize.Linq.Examples/Serialize.Linq.Examples.AspNetCore/Entities/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Serialize.Linq.Examples.AspNetCore.Entities
+{
+    [DataContract]
+    public enum Gender
+    {
+        [EnumMember]
+        Male,
+        [EnumMember]
+        Female
+    }
+}
---
+++ src/Serialize.Linq.Examples/Serialize.Linq.Examples.AspNetCore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Serialize.Linq.Examples.AspNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
