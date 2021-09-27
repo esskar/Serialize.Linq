d4743d47899bcdaec53c31b443c0340fef7b2681
OA <oahrens@bluewin.ch>
2021-09-27 19:46:42 +0200

    Improved unit test to show improvements

--- src/Serialize.Linq.Tests/New Tests/DateTimeTests.cs
+++ src/Serialize.Linq.Tests/New Tests/DateTimeTests.cs
@@ -30,40 +30,46 @@ namespace Serialize.Linq.Tests.NewTests
                 localDate = new DateTime(localDate.Ticks - (localDate.Ticks % TimeSpan.TicksPerMillisecond), localDate.Kind);
             }
             var utcDate = DateTime.SpecifyKind(localDate, DateTimeKind.Utc);
-            var realUtcDate = localDate.ToUniversalTime();
-            var testDate = localDate;
 
             Expression<Func<DateTime, bool>> localExpression = date => localDate == date && localDate.Kind == date.Kind && localDate.ToUniversalTime() == date.ToUniversalTime() && localDate.ToUniversalTime().Kind == date.ToUniversalTime().Kind;
             Expression<Func<DateTime, bool>> utcExpression = date => utcDate == date && utcDate.Kind == date.Kind && utcDate.ToUniversalTime() == date.ToUniversalTime() && utcDate.ToUniversalTime().Kind == date.ToUniversalTime().Kind;
             Expression<Func<DateTimeKind>> utcKindExpression = () => utcDate.Kind;
             Expression<Func<DateTimeKind>> localKindExpression = () => localDate.Kind;
-            Expression<Func<DateTime>> localStringExpression = () => localDate;
+            Expression<Func<DateTime>> localDateExpression = () => localDate;
+            Expression<Func<DateTime>> utcDateExpression = () => utcDate;
 
             var localValue = serializer.SerializeGeneric(localExpression);
             var utcValue = serializer.SerializeGeneric(utcExpression);
             var utcKindValue = serializer.SerializeGeneric(utcKindExpression);
             var localKindValue = serializer.SerializeGeneric(localKindExpression);
-            var localStringValue = serializer.SerializeGeneric(localStringExpression);
+            var localDateValue = serializer.SerializeGeneric(localDateExpression);
+            var utcDateValue = serializer.SerializeGeneric(utcDateExpression);
 
             var actualLocalExpression = (Expression<Func<DateTime, bool>>)serializer.DeserializeGeneric(localValue);
             var actualUtcExpression = (Expression<Func<DateTime, bool>>)serializer.DeserializeGeneric(utcValue);
             var actualLocalKindExpression = (Expression<Func<DateTimeKind>>)serializer.DeserializeGeneric(localKindValue);
             var actualUtcKindExpression = (Expression<Func<DateTimeKind>>)serializer.DeserializeGeneric(utcKindValue);
-            var actualLocalStringExpression = (Expression<Func<DateTime>>)serializer.DeserializeGeneric(localStringValue);
+            var actualLocalDateExpression = (Expression<Func<DateTime>>)serializer.DeserializeGeneric(localDateValue);
+            var actualUtcDateExpression = (Expression<Func<DateTime>>)serializer.DeserializeGeneric(utcDateValue);
 
             var localFunc = actualLocalExpression.Compile();
             var utcFunc = actualUtcExpression.Compile();
             var localKindFunc = actualLocalKindExpression.Compile();
             var utcKindFunc = actualUtcKindExpression.Compile();
-            var localStringFunc = actualLocalStringExpression.Compile();
+            var localDateFunc = actualLocalDateExpression.Compile();
+            var utcDateFunc = actualUtcDateExpression.Compile();
 
             Assert.IsFalse(localDate.ToUniversalTime() == utcDate.ToUniversalTime());
-            Assert.IsTrue(localFunc(localDate), "local date failed.");
-            Assert.IsTrue(utcFunc(utcDate), "UTC date failed.");
-            Assert.IsFalse(localFunc(utcDate), "local vs UTC date failed.");
-            Assert.IsFalse(utcFunc(localDate), "UTC vs local date failed.");
-            Assert.IsTrue(localKindFunc() == localDate.Kind, "local date kind failed.");
-            Assert.IsTrue(utcKindFunc() == utcDate.Kind, "utc date kind failed.");
+            Assert.IsTrue(localFunc(localDate), "internal comparison of local date failed.");
+            Assert.IsTrue(utcFunc(utcDate), "internal comparison of UTC date failed.");
+            Assert.IsFalse(localFunc(utcDate), "internal comparison of local vs UTC date failed.");
+            Assert.IsFalse(utcFunc(localDate), "internal comparison of UTC vs local date failed.");
+            Assert.IsTrue(localKindFunc() == localDate.Kind, "internal comparison of local date kind failed.");
+            Assert.IsTrue(utcKindFunc() == utcDate.Kind, "internal comparison of utc date kind failed.");
+            Assert.IsTrue(localDateFunc() == localDate, "return value of local date failed.");
+            Assert.IsTrue(localDateFunc().Kind == localDate.Kind, "return value of local date kind failed."); // failes in previous versions for JsonSerializer
+            Assert.IsTrue(utcDateFunc() == utcDate, "return value of UTC date failed.");
+            Assert.IsTrue(utcDateFunc().Kind == utcDate.Kind, "return value of UTC date kind failed.");
         }
     }
 }
---
+++ src/Serialize.Linq.Tests/New Tests/TimeSpanTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serialize.Linq.Serializers;
+
+namespace Serialize.Linq.Tests.NewTests
+{
+    /// <summary>
+    /// https://github.com/esskar/Serialize.Linq/issues/105
+    /// Test and fix provided by https://github.com/OlegNadymov THX!!!
+    /// </summary>
+    [TestClass]
+    public class TimeSpanTests
+    {
+
+        [TestMethod]
+        public void SerializeDeserializeTimeSpan()
+        {
+            SerializeDeserializeTimeSpanInternal(new BinarySerializer());
+            SerializeDeserializeTimeSpanInternal(new XmlSerializer());
+            SerializeDeserializeTimeSpanInternal(new JsonSerializer());
+        }
+
+        private static void SerializeDeserializeTimeSpanInternal<T>(Interfaces.IGenericSerializer<T> serializer)
+        {
+            var now = DateTime.Now;
+            var span = TimeSpan.FromSeconds(1);
+
+            Expression<Func<DateTime, bool>> expression = date => date + span > now;
+
+            var value = serializer.SerializeGeneric(expression);
+
+            var actualExpression = (Expression<Func<DateTime, bool>>)serializer.DeserializeGeneric(value);
+
+            var func = actualExpression.Compile();
+
+            Assert.IsTrue(func(now), "TimeSpan failed."); // fails in previous version for JsonSerializer
+        }
+    }
+}
