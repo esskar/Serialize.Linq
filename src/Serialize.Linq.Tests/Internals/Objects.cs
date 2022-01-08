5b1439b30eea4a09becb75ae1bf1b0df9618da1c
Sascha Kiefer <esskar@gmail.com>
2022-01-08 18:06:08 +0100

    #142 remove copyright license info

--- src/Serialize.Linq.Tests/Internals/Objects.cs
+++ src/Serialize.Linq.Tests/Internals/Objects.cs
@@ -1,12 +1,4 @@
-﻿#region Copyright
-//  Copyright, Sascha Kiefer (esskar)
-//  Released under LGPL License.
-//  
-//  License: https://raw.github.com/esskar/Serialize.Linq/master/LICENSE
-//  Contributing: https://github.com/esskar/Serialize.Linq
-#endregion
-
-namespace Serialize.Linq.Tests.Internals
+﻿namespace Serialize.Linq.Tests.Internals
 {
     public interface IFoo
     {
